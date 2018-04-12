499ef400fdea8fbe4d2937d36a7b81cf0d1e7806
nintichance <mintidance@gmail.com>
2018-04-12 13:16:32 -0400

    Added icons to easily navigate NavBar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -7,6 +7,10 @@ const NavBar = (props) => {
     return (
         <NavDiv>
             <img src="https://salesloft.com/wp-content/themes/hive/img/logo.svg" />
+            <div>
+                <Link to="/people"><img src="https://image.flaticon.com/icons/svg/179/179672.svg" width="40px"/></Link>
+                <Link to="/characters"><img src="https://image.flaticon.com/icons/svg/73/73220.svg" width="40px"/></Link>
+            </div>
         </NavDiv>
     )
 
