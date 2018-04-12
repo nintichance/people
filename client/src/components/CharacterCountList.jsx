064e020dc66f35fbb6678440a11bdad740e7c5b3
nintichance <mintidance@gmail.com>
2018-04-12 03:20:01 -0400

    Listing people in CharacterCountList

--- client/src/components/CharacterCountList.jsx
+++ client/src/components/CharacterCountList.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
 const CharacterCountList = (props) => {
+    const peopleList = props.people.map((person)=>{
+        return(
+        <div key={person.id}>
+            <h1>{person.first_name}{person.last_name}</h1>
+            <p>{person.email_address}</p>
+        </div>
+        )
+    })
     return (
         <div>
-            Hello from Character Count List
+            {peopleList}
         </div>
     )
 }
