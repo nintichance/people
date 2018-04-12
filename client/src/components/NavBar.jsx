74a5faaca9a41c2305572626f95ee1aa5bc13ff5
nintichance <mintidance@gmail.com>
2018-04-12 13:24:34 -0400

    Salesloft image in NavBar links to home

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -6,7 +6,7 @@ const NavBar = (props) => {
 
     return (
         <NavDiv>
-            <img src="https://salesloft.com/wp-content/themes/hive/img/logo.svg" />
+            <Link to="/"><img src="https://salesloft.com/wp-content/themes/hive/img/logo.svg" height="50px"/></Link>
             <div>
                 <Link to="/people"><img src="https://image.flaticon.com/icons/svg/179/179672.svg" width="40px"/></Link>
                 <Link to="/characters"><img src="https://image.flaticon.com/icons/svg/73/73220.svg" width="40px"/></Link>
