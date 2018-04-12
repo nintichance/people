dc0f51d8ffeb696da3175237a5618bcaa47f6a57
rubinj30 <jonathan.a.rubin@gmail.com>
2018-04-12 01:15:24 -0400

    Solved why HTTP request on backend wasn't working. Made a successful get request to Ruby backend with axios in React

--- app/controllers/api/people_controller.rb
+++ app/controllers/api/people_controller.rb
@@ -2,14 +2,11 @@ class Api::PeopleController < ApplicationController
     include HTTParty
     attr_accessor :facts
     def index
-        api_key = process.env.API_KEY
-        response = get('https://api.salesloft.com/v2/people.json?per_page=100', 
+        api_key = "ak_b7b6f9dbca089cec5ef461284c7dc2b85de2f1c3071134442aaadc27fd53a634"
+        @response = HTTParty.get('https://api.salesloft.com/v2/people.json?per_page=100', 
         headers: { "Content-Type": "application/json",
                     "Authorization": "Bearer #{api_key}" })
-    if response.success?
-      @people = reponse.data
-      render json: @people
-    else
-      puts "Request failed"
+            @people = @response["data"]
+            render json: @people
     end
 end
--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,33 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import axios from 'axios'
 
 class App extends Component {
+  state = {
+    people: []
+  }
+  componentWillMount(){
+    this.getPeople()
+  }
+
+  getPeople = async() => {
+    try{
+      const response = await axios.get('/api/people')
+      const people = response.data
+      this.setState({people: people})
+    }
+    catch(error){
+      console.log(error)
+    }
+  }
+
   render() {
+    console.log(this.state.people)
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        YAY!!! Hello React!!! I missed you! It's so good to be back, lovely!
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
