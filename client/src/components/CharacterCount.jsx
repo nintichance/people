d378b768bfc6689457499c90bf4f5c06067b3a64
nintichance <mintidance@gmail.com>
2018-04-12 10:38:04 -0400

    Declared a variable, which is assigned the value of an empty object. This object will hold the current key of the emailArray's current element and the value of the current count.

--- client/src/components/CharacterCount.jsx
+++ client/src/components/CharacterCount.jsx
@@ -14,6 +14,7 @@ class CharacterCount extends Component{
                 count++
                 emailArrayCopy.splice(emailArrayCopy[index], 1)
             }
+            let characterCount = {}
         })
       console.log(count)
     }
