c2781fb10939bbcb29e281d4977eaa4c1e1cccda
nintichance <mintidance@gmail.com>
2018-04-12 12:04:21 -0400

    Added basic formatting to Home component

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import { Container } from './styled-components/Containers'
 const Home = () => {
     return(
-        <div>
+        <Container>
             <Link to="/people">View People</Link>
             <Link to="/characters">Count Characters</Link>
-        </div>
+        </Container>
     )
 }
 
