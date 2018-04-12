336a251878f238fc598047fe66b8aab98ae263dc
rubinj30 <jonathan.a.rubin@gmail.com>
2018-04-12 01:37:30 -0400

    Successfully integrated React Table into PeopleTable component. Displaying people's information on the screen

--- client/src/components/PeopleTable.jsx
+++ client/src/components/PeopleTable.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
+import ReactTable from "react-table"
+import "react-table/react-table.css"
 
 const PeopleTable = (props) => {
     return(
         <div>
-            Hello from PeopleTable!
+            <ReactTable
+          data={props.people}
+          columns={[
+            {
+              Header: "Name",
+              columns: [
+                {
+                  Header: "First Name",
+                  id: "firstName",
+                  accessor: person => person.first_name
+                },
+                {
+                  Header: "Last Name",
+                  id: "lastName",
+                  accessor: person => person.last_name
+                }
+              ]
+            },
+            {
+              Header: "Info",
+              columns: [
+                {
+                  Header: "Email Address",
+                  id: "email",
+                  accessor: person => person.email_address
+                }
+              ]
+            }
+          ]}
+          defaultPageSize={10}
+          className="-striped -highlight"
+        />
+        <br />
         </div>
     )
 }
