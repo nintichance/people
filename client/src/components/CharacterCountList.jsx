ca82f5ec7695ce6ddccb398e7b4c4e17cce81bf9
nintichance <mintidance@gmail.com>
2018-04-12 03:37:53 -0400

    Added a button and passed it the email address

--- client/src/components/CharacterCountList.jsx
+++ client/src/components/CharacterCountList.jsx
@@ -1,19 +1,34 @@
-import React from 'react'
+import React, { Component } from 'react'
+import styled from 'styled-components'
 
-const CharacterCountList = (props) => {
-    const peopleList = props.people.map((person)=>{
+class CharacterCountList extends Component {
+    
+    countCharacters = (email) => {
+        alert(email)
+    }
+
+    peopleList = this.props.people.map((person)=>{
         return(
         <div key={person.id}>
-            <h1>{person.first_name}{person.last_name}</h1>
-            <p>{person.email_address}</p>
+            <p>{person.first_name} {person.last_name}</p>
+            <button onClick={() => this.countCharacters(person.email_address)}>{person.email_address}</button>
         </div>
         )
     })
+    render(){
     return (
-        <div>
-            {peopleList}
-        </div>
+        <Container>
+            <h1>Click Button to Count Unique Characters</h1>
+            {this.peopleList}
+        </Container>
     )
 }
+}
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
 
 export default CharacterCountList
