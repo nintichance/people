899498349e3a239c7f76102cfef86189672c2426
nintichance <mintidance@gmail.com>
2018-04-19 13:30:47 -0400

    Created a forEach function which iterates through the emails array. Using an if statement, each iteration checks if the duplicates array does not have  the current element in the iteration. This conditional statement also checks if the element exists in the emails array at another position in the array. If both statements return true, the current element is push into the duplicates array.

--- client/src/components/DuplicatesList.js
+++ client/src/components/DuplicatesList.js
@@ -6,10 +6,16 @@ class DuplicatesList extends Component{
     }
     findDuplicates = () => {
         const duplicates = []
-        const emails = this.props.people.map((person)=>{
-            return person.email_address
-        })
-        console.log(emails)
+        // const emails = this.props.people.map((person)=>{
+        //     return person.email_address
+        // })
+        const emails = ["nintidance@gmail.com", "myemail@emailaddress.com", "nintichance@gmail.com", "sinbadscat@email.com", "findingit@hufflepuff.com", "myemail@emailaddress.com", "nintidance@gmail.com", "ohworm@gmail.com", "ninntichanc@gmail.com", "nintidance@gmail.com" ]
+        emails.forEach((email, index) => {
+            if((duplicates.indexOf(email) === -1) && (emails.indexOf(email, index + 1) !== -1)){
+              duplicates.push(email)
+            }
+          })
+        console.log(duplicates)
     }
     render(){
     return(
