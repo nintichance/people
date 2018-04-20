4819eaac9756ec830656947afd90561a2401a054
nintichance <mintidance@gmail.com>
2018-04-20 11:22:22 -0400

    This expression iterates through the first array, checking to see if each index of the first array matches the corresponding indexes in the second array. Because objects cannot be compared, it gets the value of the key and the value of the respective value in the object at that current iteration and compares it to the second array's object's key/value pair.

--- client/src/components/DuplicatesList.js
+++ client/src/components/DuplicatesList.js
@@ -17,12 +17,24 @@ class DuplicatesList extends Component{
         console.log(this.props.people.length)
     }
     estimateDuplicates = (arr1, arr2) => {
+        let count = 0
         //compare array one with array two
         //iterate through each array
-        //compare the indexes of the array, if they are the same, then add to the count
+        //compare the elements of the array, if they are the same, then add to the count
+        arr1.forEach((element, index) => {
+            console.log(element)
+              if ((Object.keys(element)[0] === Object.keys(arr2[index])[0]) && (Object.values(element)[0] === Object.values(arr2[index])[0])){
+                  count += 1
+              }
+          })
         //take the length of the longer array and the count. divide the count by it
+        if (arr1.length === arr2.length){
+            return count/arr1.length
+        } 
+        else if (arr1.length > arr2.length){
+            return count/arr1.length
+        }
         //if accuracy is more than 90%, then it's a duplicate
-        
       }
     render(){
     return(
